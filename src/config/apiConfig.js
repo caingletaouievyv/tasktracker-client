1324f16c4cc3284862c8fc4b76565618c9e997c9
Awi <caingletaouievyv@gmail.com>
2025-07-22 14:29:54 +0800

    Add apiConfig.js with env fallback for Netlify part 3

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 // src/config/apiConfig.js
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://localhost:7010/api";
 
 export default API_BASE_URL;
