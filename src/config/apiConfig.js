65220d53b8f6304dec92ffa3f41bb979186ba72f
Awi <caingletaouievyv@gmail.com>
2025-07-22 14:12:59 +0800

    Add apiConfig.js with env fallback for Netlify

---
+++ src/config/apiConfig.js
@@ -0,0 +1,6 @@
+// src/config/apiConfig.js
+
+const API_BASE_URL = "https://localhost:7010/api"; //local
+// const API_BASE_URL = "https://tasktracker-api-aud9ehbmf6d8agej.canadacentral-01.azurewebsites.net/api"; //azure
+
+export default API_BASE_URL;
